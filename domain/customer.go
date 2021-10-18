028c7fff7b0e86a163179ed45f1daacbc8e1ea5a
Jungsoo Kim <hollibleling12@gmail.com>
2021-10-18 10:37:00 +0900

    chore: customer model field name changed

--- domain/customer.go
+++ domain/customer.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Customer struct {
-	Id           uuid.UUID `gorm:"type:char(36);primaryKey"`
-	Name         string    `gorm:"size:320;index;not null"`
-	ChannelName  string    `gorm:"size:100;index;not null"`
-	ChannelLink  string    `gorm:"size:2048;not null"`
-	Email        string    `gorm:"size:320;index;not null"`
-	Mobile       string    `gorm:"size:24;index;not null"`
-	OrderableCnt int       `gorm:"column:orderable_count;not null"`
-	PersonaLink  string    `gorm:"size:2048;not null"`
-	OnedriveLink string    `gorm:"size:2048;not null"`
-	Memo         string    `gorm:"type:text;not null"`
+	Id             uuid.UUID `gorm:"type:char(36);primaryKey"`
+	Name           string    `gorm:"size:320;index;not null"`
+	ChannelName    string    `gorm:"size:100;index;not null"`
+	ChannelLink    string    `gorm:"size:2048;not null"`
+	Email          string    `gorm:"size:320;index;not null"`
+	Mobile         string    `gorm:"size:24;index;not null"`
+	OrderableCount int       `gorm:"column:orderable_cnt;not null"`
+	PersonaLink    string    `gorm:"size:2048;not null"`
+	OnedriveLink   string    `gorm:"size:2048;not null"`
+	Memo           string    `gorm:"type:text;not null"`
 }
