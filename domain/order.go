06b48fa8e67575238161d0beb817bd371307efcd
Chi heon Song <hrpp1353@gmail.com>
2021-10-20 14:47:08 +0900

    refactor: DueDate와 Assignee에 대해 nullable이기 때문에 포인터 지정을 해줌

--- domain/order.go
+++ domain/order.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Order struct {
-	Id        uuid.UUID `gorm:"type:char(36);primaryKey"`
-	OrderedAt time.Time `gorm:"size:6;index;not null"`
-	Orderer   string    `gorm:"size:36;not null"`
-	EditCount uint16    `gorm:"default:0;not null"`
-	EditTotal uint16    `gorm:"default:2;not null"`
-	DueDate   time.Time `gorm:"size:6"`
-	Assignee  string    `gorm:"size:36"`
+	Id        uuid.UUID  `gorm:"type:char(36);primaryKey"`
+	OrderedAt time.Time  `gorm:"size:6;index;not null"`
+	Orderer   string     `gorm:"size:36;not null"`
+	EditCount uint16     `gorm:"not null"`
+	EditTotal uint16     `gorm:"not null"`
+	DueDate   *time.Time `gorm:"size:6"`
+	Assignee  *string    `gorm:"size:36"`
 }
 
 func MakeOrder(orderer string) Order {
@@ -21,6 +21,7 @@ func MakeOrder(orderer string) Order {
 		Id:        uuid.New(),
 		OrderedAt: time.Now(),
 		Orderer:   orderer,
-		DueDate:   time.Now(),
+		DueDate:   nil,
+		Assignee:  nil,
 	}
 }
