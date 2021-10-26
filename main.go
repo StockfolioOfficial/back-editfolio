4783d1dcba92a602dd2b12363051ecbc7f6fcead
Lee Jaeseong <fg9658@gmail.com>
2021-10-26 22:40:20 +0900

    fix: docker COPY 경로 변경

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ package main
 // @in header
 // @name Authorization
 
-// todo termsOfService 변경 필요
-
 // @title 에딧폴리오 public API
 // @version 1.0
 // @description 설명문 작성해야함
@@ -18,7 +16,6 @@ package main
 // @license.name MIT License
 // @license.url https://en.wikipedia.org/wiki/MIT_License
 
-// todo 호스트 및 http https 프로토콜 변경 루틴 추가 필요
 // @host localhost:8000
 // @BasePath /
 func main() {
