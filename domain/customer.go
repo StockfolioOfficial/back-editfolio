2a7e5163c3353af7dac0c34d4c4d869e2246d07f
Jungsoo Kim <hollibleling12@gmail.com>
2021-10-27 14:50:33 +0900

    chore: customer model changed_2

--- domain/customer.go
+++ domain/customer.go
@@ -9,18 +9,18 @@ import (
 )
 
 type Customer struct {
-	Id             uuid.UUID `gorm:"type:char(36);primaryKey"`
-	Name           string    `gorm:"size:320;index;not null"`
-	ChannelName    string    `gorm:"size:100;index;not null"`
-	ChannelLink    string    `gorm:"size:2048;not null"`
-	Email          string    `gorm:"size:320;index;not null"`
-	Mobile         string    `gorm:"size:24;index;not null"`
-	OrderableCount int       `gorm:"column:orderable_cnt"`
-	PersonaLink    string    `gorm:"size:2048;not null"`
-	OnedriveLink   string    `gorm:"size:2048;not null"`
-	Memo           string    `gorm:"type:text"`
-	DescribeStart  time.Time `gorm:"size:6"`
-	DescribeEnd    time.Time `gorm:"size:6"`
+	Id             uuid.UUID  `gorm:"type:char(36);primaryKey"`
+	Name           string     `gorm:"size:320;index;not null"`
+	ChannelName    string     `gorm:"size:100;index;not null"`
+	ChannelLink    string     `gorm:"size:2048;not null"`
+	Email          string     `gorm:"size:320;index;not null"`
+	Mobile         string     `gorm:"size:24;index;not null"`
+	OrderableCount int        `gorm:"column:orderable_cnt"`
+	PersonaLink    string     `gorm:"size:2048;not null"`
+	OnedriveLink   string     `gorm:"size:2048;not null"`
+	Memo           string     `gorm:"type:text"`
+	SubscribeStart *time.Time `gorm:"size:6"`
+	SubscribeEnd   *time.Time `gorm:"size:6"`
 }
 
 func (Customer) TableName() string {
